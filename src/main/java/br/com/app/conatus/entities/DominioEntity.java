40d27069ad1ddb7c3b258ef0e1f9dde72bad8246
jonassantos1000 <jonasvale3@hotmail.com>
2023-10-23 21:28:39 -0300

    fix: ajuste nome da coluna tipo na tabela dominio

--- src/main/java/br/com/app/conatus/entities/DominioEntity.java
+++ src/main/java/br/com/app/conatus/entities/DominioEntity.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -35,6 +36,7 @@ public class DominioEntity implements Serializable {
 	private String codigo;
 	
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ID_TIPO")
 	private DominioTipoEntity tipo;
 	
 }
